88c1a8c7c902cdda272c98d1745b34801ae2e613
durante-stephanie <durantestephanie07@gmail.com>
2024-12-19 00:20:17 +0800

    add changes to case 1 (#12)
    * add case 1
    
    * add changes to case 1

--- main.py
+++ main.py
@@ -21,19 +21,17 @@ def process_choice(choice):
         case 1: 
             from herchel import durante
 
-            durante.FitnessTracker.display_menu()
+            durante.FitnessTracker().display_menu()
         case 2:
-            from herchel.florido import Christmas
-            christmas = Christmas()
-            christmas.display_menu()
+            from herchel import florido
+            
+            florido.Christmas.display_menu()
         case 3:
             from herchel.riomalos import KDramaList
             kdramalist = KDramaList()
             kdramalist.display_menu()
         case 4:
-            from herchel import siervo
-
-            siervo.GradeManagementSystem.display_menu()
+            # TODO(Siervo): call the menu in your module
         case 5:
             pass
         case _:
